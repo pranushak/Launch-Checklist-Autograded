512969931e3481ac9276dfd3962a995e858ef251
Pranusha <narsicloud@gmail.com>
2024-08-18 23:24:52 -0500

    updated all requirements

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ window.addEventListener("load", function() {
     console.log(listedPlanets);
     let form = document.getElementById("launchForm");
     form.addEventListener("submit",function(event){
+        event.preventDefault();
         let pilotName = document.querySelector("input[name=pilotName]").value;
         let copilotName = document.querySelector("input[name=copilotName]").value;
         let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
@@ -20,15 +21,32 @@ window.addEventListener("load", function() {
         let valid = formSubmission(document,listedPlanets,pilotName, copilotName, fuelLevel, cargoMass);
         if(valid) {
             let faultForm = document.getElementById("faultyItems");
-            faultForm.style.visibility = 'visible'
             let pilotId = document.getElementById("pilotStatus");
             let copilotId = document.getElementById("copilotStatus");
+            let fuelId = document.getElementById("fuelStatus");
+            let h2Id = document.getElementById("launchStatus");
+            let cargoId = document.getElementById("cargoStatus");
+            faultForm.style.visibility = 'hidden';
             pilotId.innerHTML = `Pilot ${pilotName} is ready for launch`;
             copilotId.innerHTML = `Copilot ${copilotName} is ready for launch`;
-        } else {
-            faultForm.style.visibility = 'hidden';
-        }
-        event.preventDefault();
+            fuelId.innerHTML = `Fuel level high enough for launch`;
+            cargoId.innerHTML = `Cargo mass low enough for launch`;
+            h2Id.innerHTML = `Awaiting Information Before Launch`;
+            h2Id.style.color = "";
+            if(fuelLevel < 10000){
+                fuelId.innerHTML = `Fuel level too low for launch`;
+                h2Id.innerHTML = `Shuttle Not Ready for Launch`;
+                h2Id.style.color = "red";
+                faultForm.style.visibility = 'visible';
+            }
+            if(cargoMass > 10000){
+                cargoId.innerHTML = `Cargo mass is too much for the shuttle to take off`;
+                h2Id.innerHTML = `Shuttle Not Ready for Launch`;
+                h2Id.style.color = "red";
+                faultForm.style.visibility = 'visible';
+            }
+        } 
+        
     })
     
  });
