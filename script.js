3a96686dc5f3ca3adaed3063b97084b3e0af1bc3
Pranusha <narsicloud@gmail.com>
2024-08-21 15:45:08 -0500

    Checklist-Form-Autograded

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Write your JavaScript code here!
-
 window.addEventListener("load", function () {
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
@@ -13,13 +12,13 @@ window.addEventListener("load", function () {
     })
     let form = document.getElementById("launchForm");
     form.addEventListener("submit", function (event) {
-        event.preventDefault();
         let pilotName = document.querySelector("input[name=pilotName]").value;
         let copilotName = document.querySelector("input[name=copilotName]").value;
         let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
         let cargoMass = document.querySelector("input[name=cargoMass]").value;
         let list = document.getElementById("faultyItems");
-        let valid = formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass);
+        formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass);
+        event.preventDefault();
     })
 
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -33,60 +33,59 @@ function validateInput(testInput) {
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    let valid = true;
+    let validData = true;
     const pilotValue = validateInput(pilot);
     const copilotValue = validateInput(copilot);
     const fuelValue = validateInput(fuelLevel);
     const cargoValue = validateInput(cargoLevel);
+    let pilotId = document.getElementById("pilotStatus");
+    let copilotId = document.getElementById("copilotStatus");
     let fuelId = document.getElementById("fuelStatus");
     let cargoId = document.getElementById("cargoStatus");
     let h2Id = document.getElementById("launchStatus");
-    let pilotId = document.getElementById("pilotStatus");
-    let copilotId = document.getElementById("copilotStatus");
     list.style.visibility = 'hidden';
     if (pilotValue === "Is a Number"
         || copilotValue === "Is a Number"
         || fuelValue === "Not a Number"
         || cargoValue === "Not a Number") {
         alert("Make sure to enter valid information for each field!");
-    } else if (pilotValue === "Empty" ||
+        validData = false;
+    } else if(pilotValue === "Empty" ||
         copilotValue === "Empty" ||
         fuelValue === "Empty" ||
         cargoValue === "Empty") {
         alert("All fields are required!");
+        validData = false; 
     }
-    if (fuelLevel < 10000) {
-        fuelId.innerHTML = `Fuel level too low for launch`;
-        h2Id.innerHTML = `Shuttle Not Ready for Launch`;
+    if(validData) {
+        let fuelLevelValid = true;
+        let cargoLevelValid = true;
         pilotId.innerHTML = `Pilot ${pilot} is ready for launch`;
         copilotId.innerHTML = `Co-pilot ${copilot} is ready for launch`;
-        h2Id.style.color = "red";
-        list.style.visibility = 'visible';
-        valid = false;
-    } else {
-        fuelId.innerHTML = `Fuel level high enough for launch`;
+        if (fuelLevel < 10000) {
+            fuelId.innerHTML = `Fuel level too low for launch`;
+            fuelLevelValid = false;
+        } else {
+            fuelId.innerHTML = `Fuel level high enough for launch`;
+        }
+        if (cargoLevel > 10000) {
+            cargoId.innerHTML = `Cargo mass too heavy for launch`;
+            cargoLevelValid = false;
+        } else {
+            cargoId.innerHTML = `Cargo mass low enough for launch`;
+        }
+        if(cargoLevelValid && fuelLevelValid) {
+            fuelId.innerHTML = `Fuel level high enough for launch`;
+            cargoId.innerHTML = `Cargo mass low enough for launch`;
+            h2Id.innerHTML = `Shuttle is Ready for Launch`;
+            h2Id.style.color = "green";
+            list.style.visibility = 'visible';
+        } else {
+            h2Id.innerHTML = `Shuttle Not Ready for Launch`;
+            list.style.visibility = 'visible';
+            h2Id.style.color = "red";
+        }
     }
-    if (cargoLevel > 10000) {
-        cargoId.innerHTML = `Cargo mass too heavy for launch`;
-        h2Id.innerHTML = `Shuttle Not Ready for Launch`;
-        pilotId.innerHTML = `Pilot ${pilot} is ready for launch`;
-        copilotId.innerHTML = `Co-pilot ${copilot} is ready for launch`;
-        h2Id.style.color = "red";
-        list.style.visibility = 'visible';
-        valid = false
-    } else {
-        cargoId.innerHTML = `Cargo mass low enough for launch`;
-    }
-    if (valid) {
-        fuelId.innerHTML = `Fuel level high enough for launch`;
-        cargoId.innerHTML = `Cargo mass low enough for launch`;
-        pilotId.innerHTML = `Pilot ${pilot} is ready for launch`;
-        copilotId.innerHTML = `Co-pilot ${copilot} is ready for launch`;
-        h2Id.innerHTML = `Shuttle is Ready for Launch`;
-        h2Id.style.color = "green";
-        list.style.visibility = 'visible';
-    }
-    return valid;
 }
 
 async function myFetch() {
